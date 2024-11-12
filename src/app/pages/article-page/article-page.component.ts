ca644713edd790864e64dcd4c4021b459cf40b3e
sebastienDOC <s.dechand21@gmail.com>
2024-11-12 16:15:52 +0100

    feat(): add access to data with httpclient

--- src/app/pages/article-page/article-page.component.ts
+++ src/app/pages/article-page/article-page.component.ts
@@ -1,21 +1,32 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Article } from '../../models/article.model';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-article-page',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './article-page.component.html',
-  styleUrl: './article-page.component.scss'
+  styleUrls: ['./article-page.component.scss']
 })
-export class ArticlePageComponent {
-  private route: ActivatedRoute = inject(ActivatedRoute);
+export class ArticlePageComponent implements OnInit {
+  private route = inject(ActivatedRoute);
+  private http = inject(HttpClient);
   articleId!: number;
+  article?: Article;
 
   ngOnInit() {
     this.route.paramMap.subscribe((params: ParamMap) => {
       this.articleId = Number(params.get('id'));
+      this.getArticleById(this.articleId);
+    });
+  }
+
+  getArticleById(id: number): void {
+    this.http.get<Article>(`http://localhost:3000/articles/${id}`).subscribe((article) => {
+      this.article = article;
     });
   }
 }
-
